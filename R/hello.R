8c242195857a323b1b7d1741b4007e7bafe95bfe
Raphael Fix <raphifix@gmail.com>
2016-08-04 22:38:49 -0700

    Added get_user function

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -15,4 +15,22 @@
 
 hello <- function() {
   print("Hello, world!")
+}
+
+get_user <- function(client_id, user_id, by_username = TRUE){
+  if(by_username == TRUE){
+    username_lookup <- httr::GET(paste0("http://api.soundcloud.com/resolve?url=http://soundcloud.com/",user_id,"&client_id=",client_id))
+    user_id <- httr::content(username_lookup, as = 'parsed')$id
+  }
+  resp <- httr::GET(paste0("http://api.soundcloud.com/users/",user_id,"?client_id=",client_id))
+  resp <- httr::content(resp, as = 'parsed')
+  if(length(resp$errors[[1]]$error_message)){
+    print('404. User not found.')
+  }else if(is.null(resp)){
+    print('Empty Response. Check your client ID.')
+  }else{
+    resp <- as.data.frame(unlist(resp))
+    names(resp) <- 'User'
+    return(resp)
+  }
 }
